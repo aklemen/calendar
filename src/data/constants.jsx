6cf703dc77d0a6918aae31f3ab8304b9c01263eb
aklemen <antonklemen@gmail.com>
2021-03-23 00:17:40 +0100

    Get dates methods

---
+++ src/data/constants.jsx
@@ -0,0 +1,10 @@
+import holidaysData from '../data/holidays.json';
+
+export const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'
+];
+
+export const days = [
+    'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+];
