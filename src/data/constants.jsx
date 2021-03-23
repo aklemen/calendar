63ac1ae3a5d65fd00860fb01c120e930c7270021
aklemen <antonklemen@gmail.com>
2021-03-23 16:26:36 +0100

    Fix import

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -1,5 +1,3 @@
-import holidaysData from '../data/holidays.json';
-
 export const months = [
     'January', 'February', 'March', 'April', 'May', 'June', 'July',
     'August', 'September', 'October', 'November', 'December'
